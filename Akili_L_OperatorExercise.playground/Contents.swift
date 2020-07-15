81309808552497ed2a87feab8162675d70087695
London2021 <67976260+London2021@users.noreply.github.com>
2020-07-15 11:13:01 -0500

    OperatorExercise Commit Changes
    OperatorExercise Commit Changes

---
+++ Akili_L_OperatorExercise.playground/Contents.swift
@@ -0,0 +1,21 @@
+import Cocoa
+
+// Define all input values
+var a = 0
+var b = 0
+var resultAdd = 0
+var resultSub = 0
+var resultMultiply = 0
+var resultDivision = 0
+
+// Define all operation
+resultAdd = a + b
+resultSub = a - b
+resultMultiply = a * b
+resultDivision = a / b
+
+// Print all the results
+print ("The result of adding is \(resultAdd)")
+print ("The result of subtacting is \(resultSub)")
+print ("The result of multiplying is \(resultMultiply)")
+print ("The result of divding is \(resultDivision)")
